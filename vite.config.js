3ce192474a0234b53a354306dcf388c8f9c2d841
williamtanuwijaya <will.tw0807@gmail.com>
2025-12-25 19:59:18 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/postcss';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // PENTING: Ganti base sesuai nama repo.
+  // Jika repo Anda bernama "williamtanuwijaya.github.io", isi '/'
+  // Jika repo Anda bernama "portfolio", isi '/portfolio/'
+  base: '/portfolio-william/',
+  build: {
+    outDir: 'docs', // <-- INI KUNCINYA (Ubah output ke folder docs)
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+});
